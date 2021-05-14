513954e342b4a180a2b93c57f242300b036bb2ce
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-05-14 17:03:56 +0200

    Chnage order of width (x) and height (y)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   var width = req.query.w || '450px';
   var height = req.query.h || '300px';
   var text =
-    req.query.text || (req.query.h || '450') + ' x ' + (req.query.w || '300');
+    req.query.text || (req.query.w || '300') + ' x ' + (req.query.h || '450');
   var background = req.query.background || '#4c4c4c';
   var color = req.query.color || '#fff';
   var border = req.query.border || background;
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 
   res.contentType('image/svg+xml');
   res.send(
-    `<?xml version="1.0"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${width}" height="${height}"><rect x="0" y="0" width="${width}" height="${height}" stroke="${border}" stroke-width="${borderWidth}" fill="${background}"/><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${color}">${text}</text></svg>`
+    `<?xml version="1.0"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${width}" height="${height}"><rect x="0" y="0" width="${width}" height="${height}" stroke="${border}" stroke-width="${borderWidth}" fill="${background}"/><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${color}" font-family="Open Sans, sans-serif">${text}</text></svg>`
   );
 });
 
